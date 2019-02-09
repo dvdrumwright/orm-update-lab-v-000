9eb8fddc8e41e4a93aac55f2ee4bf87734d4e157
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-02-09 21:40:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,19 +27,18 @@ def self.drop_table
 end
 
 def save
-    if self.id
-      self.update
-    else
-      sql = <<-SQL
-        INSERT INTO students (name, grade)
-        VALUES (?, ?)
-      SQL
+ if self.id
+    self.update
+ else
+   sql = <<-SQL
+    INSERT INTO students (name, grade)
+    VALUES (?, ?)
+    SQL
 
-      DB[:conn].execute(sql, self.name, self.grade)
-
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-    end
+  DB[:conn].execute(sql, self.name, self.grade)
+   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
+end
 
 def self.create(name, grade)
   students = Student.new(name, grade)
@@ -55,8 +54,7 @@ def self.create(name, grade)
 
  def self.find_by_name(name)
    sql = <<-SQL
-   SELECT * students
-   FROM students WHERE name = ?
+   SELECT * FROM students WHERE name = ?
    LIMIT 1
    SQL
 
