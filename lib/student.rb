e545e99f4d52879bafc0f5c6335fbca6ea5a5df8
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-02-09 21:18:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ def save
 
       DB[:conn].execute(sql, self.name, self.grade)
 
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][1]
     end
 
   end
