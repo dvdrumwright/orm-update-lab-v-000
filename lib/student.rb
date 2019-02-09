9fb2e1f0d49a2514300e404adc46df941fba59cc
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-02-09 21:47:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,23 +45,45 @@ def self.create(name, grade)
    students.save
  end
 
- def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    grade = row[2]
-    self.new(id, name, grade)
-  end
-
- def self.find_by_name(name)
-   sql = <<-SQL
-   SELECT * FROM students WHERE name = ?
-   LIMIT 1
-   SQL
+ # def self.new_from_db(row)
+ #    id = row[0]
+ #    name = row[1]
+ #    grade = row[2]
+ #    self.new(id, name, grade)
+ #  end
+ #
+ # def self.find_by_name(name)
+ #   sql = <<-SQL
+ #   SELECT * FROM students WHERE name = ?
+ #   LIMIT 1
+ #   SQL
 
 DB[:conn].execute(sql,name).map |r|
  self.new_from_db(r)
  end.first
 end
+
+def self.find_by_name(name)
+    # find the student in the database given a name
+    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+
+
+
+
+
+
 
 
 def update
