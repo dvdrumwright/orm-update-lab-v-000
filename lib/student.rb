7ab8fb7b8c2090e25471d310cfc98c0185ac5ba2
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-02-09 20:02:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,6 +13,17 @@ class Student
     @grade = grade
   end
 
+  def self.create_table_student
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students ( id INTEGER PRIMARY KEY, name TEXT, grade TEXT ) SQL
+    DB[:conn].execute(sql)
+  end
+
+
+
+
+
+
 
 
 
