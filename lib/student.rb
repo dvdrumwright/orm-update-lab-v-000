a1b326edce6d72caeafa83b0749e9c22aa6d3c10
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-02-09 19:38:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,14 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  attr_accessor :name, :grade, :id
+
+  def initialized(name:,grade:,id:=nil)
+      @name = name
+      @grade = grade
+      @id = id
+    end 
+
+
 
 end
